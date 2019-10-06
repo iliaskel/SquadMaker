70719d2407343c005ac4e84d32df30a2a11c9ae5
kelasov <ikelasid@gmail.com>
2019-10-06 13:11:18 +0300

    Update db

--- app/src/main/java/com/example/squadmaker/model/database/SquadDatabase.kt
+++ app/src/main/java/com/example/squadmaker/model/database/SquadDatabase.kt
@@ -5,15 +5,21 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.squadmaker.model.database.dao.CharactersDao
+import com.example.squadmaker.model.database.dao.ComicsDao
 import com.example.squadmaker.model.database.dao.DetailedCharacterDao
 import com.example.squadmaker.model.database.dao.SquadDao
 import com.example.squadmaker.model.database.entity.CharacterEntity
+import com.example.squadmaker.model.database.entity.ComicsEntity
 import com.example.squadmaker.model.database.entity.DetailedCharacterEntity
 import com.example.squadmaker.model.database.entity.SquadEntity
 
 @Database(
-    entities = [SquadEntity::class, DetailedCharacterEntity::class, CharacterEntity::class],
-    version = 1,
+    entities = [
+        SquadEntity::class,
+        DetailedCharacterEntity::class,
+        CharacterEntity::class,
+        ComicsEntity::class],
+    version = 2,
     exportSchema = false
 )
 abstract class SquadDatabase : RoomDatabase() {
@@ -21,6 +27,7 @@ abstract class SquadDatabase : RoomDatabase() {
     abstract fun charactersDao(): CharactersDao
     abstract fun squadDao(): SquadDao
     abstract fun detailedCharacterDao(): DetailedCharacterDao
+    abstract fun comicsDao(): ComicsDao
 
     companion object {
 
