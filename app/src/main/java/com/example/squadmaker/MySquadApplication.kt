0d0c5bac60dc80b60d149706141aec4e9648cae4
Ilias Kelasidis <ilias.kelasidis@uxma.com>
2020-03-08 13:44:43 +0100

    Extend the Application class // start koin

---
+++ app/src/main/java/com/example/squadmaker/MySquadApplication.kt
@@ -0,0 +1,21 @@
+package com.example.squadmaker
+
+import android.app.Application
+import com.example.squadmaker.di.networkModule
+import com.example.squadmaker.di.repositoryModule
+import com.example.squadmaker.di.viewModelsModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MySquadApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MySquadApplication)
+            modules(listOf(networkModule, repositoryModule, viewModelsModule))
+        }
+    }
+}
