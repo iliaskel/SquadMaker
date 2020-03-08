d3e0ba88d6043073e11d9f663d58f8c81154a16b
Ilias Kelasidis <ilias.kelasidis@uxma.com>
2020-03-08 13:48:06 +0100

    Update creation of repository to get it as external dependency to Main/Detailed ViewModel // Add interfaces for Main/Detailed ViewModel

--- app/src/main/java/com/example/squadmaker/viewmodel/DetailedViewModel.kt
+++ app/src/main/java/com/example/squadmaker/viewmodel/DetailedViewModel.kt
@@ -1,59 +1,7 @@
 package com.example.squadmaker.viewmodel
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
-import com.example.squadmaker.model.Repository
-import com.example.squadmaker.model.database.entity.ComicsEntity
-import com.example.squadmaker.model.database.entity.DetailedCharacterEntity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-
-class DetailedViewModel(application: Application) : AndroidViewModel(application) {
-
-    // region Fields
-
-    private val repository = Repository.getInstance(application)
-
-    //end region
-
-    // region Public Functions
-
-    fun updateDetailedCharacter(id: Int) {
-        viewModelScope.launch(Dispatchers.IO) {
-            withContext(Dispatchers.IO) {
-                repository.fetchAndSaveDetailedCharacterById(id)
-                repository.fetchAndSaveComicsByCharacterId(id)
-            }
-        }
-    }
-
-    fun removeDetailedCharacter() {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.removeDetailedCharacter()
-        }
-    }
-
-    fun updateSquadList(isSquadMember: Boolean) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.updateSquadEntry(isSquadMember)
-        }
-    }
-
-    // endregion
-
-    // region LiveData observing
-
-    fun getDetailedCharacter(): LiveData<DetailedCharacterEntity> {
-        return repository.getDetailedCharacter()
-    }
-
-    fun getComics(): LiveData<List<ComicsEntity>> {
-        return repository.getComics()
-    }
-
-    // endregion
-
+interface DetailedViewModel {
+    fun updateDetailedCharacter(id: Int)
+    fun removeDetailedCharacter()
+    fun updateSquadList(isSquadMember: Boolean)
 }
